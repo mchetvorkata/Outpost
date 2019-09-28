ccaf03af813be4080baa001fc05c29e7fe390606
Jamal <pinkpanther.naidenov8@gmail.com>
2019-09-28 16:39:27 +0300

    Add files via upload

---
+++ src/main/java/com/outpost/frozenworld/Outpost.java
@@ -0,0 +1,31 @@
+package com.outpost.frozenworld;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Outpost.MODID, name = Outpost.NAME, version = Outpost.VERSION)
+public class Outpost
+{
+    public static final String MODID = "outpostmod";
+    public static final String NAME = "Outpost31";
+    public static final String VERSION = "@VERSION";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
